86aded9cee287d8d85c5e258083904fb124e6e5e
Ivan Zuev <i-zuev@yandex-team.ru>
2021-12-22 14:54:23 +0300

    feat!: remove unused legacy code and dependencies

--- legacy/lib/serverless/functions/v1/invoke.d.ts
+++
@@ -1,9 +0,0 @@
-import { ChannelCredentials } from 'grpc';
-import { Session } from '../../../../../src';
-
-export class InvokeService {
-    constructor(session?: Session);
-
-    invoke(functionId: string, payload?: any): Promise<object>;
-    wrap(functionId: string): (payload?: any) => Promise<object>;
-}
