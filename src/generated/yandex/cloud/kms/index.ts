a7e0a15bbb35087446e8fb773e782ebe9539fae4
Ivan Zuev <i-zuev@yandex-team.ru>
2021-12-23 15:50:22 +0300

    feat: generate exports for all entities in cloud api
    BREAKING CHANGE

--- src/generated/yandex/cloud/kms/index.ts
+++ src/generated/yandex/cloud/kms/index.ts
@@ -1,2 +1,3 @@
-export * as symmetric_crypto from './v1/symmetric_crypto_service'
-export * as symmetric_key from './v1/symmetric_key_service'
+export * as symmetric_crypto_service from './v1/symmetric_crypto_service'
+export * as symmetric_key from './v1/symmetric_key'
+export * as symmetric_key_service from './v1/symmetric_key_service'
---
+++ src/generated/yandex/cloud/quota/index.ts
@@ -0,0 +1,1 @@
+export * as quota from './quota'
