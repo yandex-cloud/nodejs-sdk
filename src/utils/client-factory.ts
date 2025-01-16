592fef54044f53837a009b53f2375257e3441038
GermanVor <vorotnikov.german@yandex.ru>
2025-01-16 16:00:43 +0100

    chore: add ClientCallArgs to export

--- src/utils/client-factory.ts
+++ src/utils/client-factory.ts
@@ -1,9 +1,11 @@
-import { createClientFactory } from 'nice-grpc';
-import { deadlineMiddleware } from 'nice-grpc-client-middleware-deadline';
-import { retryMiddleware } from '../middleware/retry';
+import { ClientFactory, createClientFactory } from 'nice-grpc';
+import { deadlineMiddleware, DeadlineOptions } from 'nice-grpc-client-middleware-deadline';
+import { retryMiddleware, RetryOptions } from '../middleware/retry';
 import { errorMetadataMiddleware } from '../middleware/error-metadata';
 
-export const clientFactory = createClientFactory()
+export type ClientCallArgs = RetryOptions | DeadlineOptions;
+
+export const clientFactory: ClientFactory<ClientCallArgs> = createClientFactory()
     .use(errorMetadataMiddleware)
     .use(retryMiddleware)
     .use(deadlineMiddleware);
