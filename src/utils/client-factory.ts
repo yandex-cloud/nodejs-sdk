e6981dc96043a72fa1a97676fe01555ea2c5181b
Ivan Zuev <dajohnes@gmail.com>
2026-04-29 15:28:04 +0300

    feat: implement otel support

--- src/utils/client-factory.ts
+++ src/utils/client-factory.ts
@@ -1,11 +1,23 @@
-import { ClientFactory, createClientFactory } from 'nice-grpc';
+import { ClientFactory, ClientMiddleware, createClientFactory } from 'nice-grpc';
 import { deadlineMiddleware, DeadlineOptions } from 'nice-grpc-client-middleware-deadline';
 import { retryMiddleware, RetryOptions } from '../middleware/retry';
 import { errorMetadataMiddleware } from '../middleware/error-metadata';
 
 export type ClientCallArgs = RetryOptions & DeadlineOptions;
 
-export const clientFactory: ClientFactory<ClientCallArgs> = createClientFactory()
-    .use(errorMetadataMiddleware)
-    .use(retryMiddleware)
-    .use(deadlineMiddleware);
+export const buildClientFactory = (
+    extraMiddlewares: ClientMiddleware[] = [],
+): ClientFactory<ClientCallArgs> => {
+    let factory: ClientFactory<ClientCallArgs> = createClientFactory()
+        .use(errorMetadataMiddleware)
+        .use(retryMiddleware)
+        .use(deadlineMiddleware);
+
+    for (const mw of extraMiddlewares) {
+        factory = factory.use(mw) as ClientFactory<ClientCallArgs>;
+    }
+
+    return factory;
+};
+
+export const clientFactory: ClientFactory<ClientCallArgs> = buildClientFactory();
